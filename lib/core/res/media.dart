7422b00b9735d36be45fad7b62bfde337f22b964
David Simeon <daviddeveloper20@gmail.com>
2024-09-19 00:07:31 +0100

    Completed the main setup of the program

---
+++ lib/core/res/media.dart
@@ -0,0 +1,10 @@
+
+
+abstract class Media {
+
+  static const _baseIcon = "assets/icons";
+  static const _baseImages = "assets/images";
+  static const _baseSvg = "assets/svgs";
+
+  //splash screen icon
+}
